8a8148fc6a0c80dc2632cf54b58ef19ee7e6fe4d
Woraphot Chokratanasombat <bee.w@excelbangkok.com>
2019-03-22 14:46:47 +0700

    [typings] Change: Export more types

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,14 @@
-import { PhotoManipulatorStatic } from './lib/PhotoManipulatorTypes';
+import { Color, ImageSource, PhotoBatchOperations, PhotoManipulatorStatic, Point, Size, TextOptions } from './lib/PhotoManipulatorTypes';
 
+// Type
+export type Color = Color
+export type ImageSource = ImageSource
+export type PhotoBatchOperations = PhotoBatchOperations
+export type Point = Point
+export type Size = Size
+export type TextOptions = TextOptions
+
+// Native Module
 declare var PhotoManipulator: PhotoManipulatorStatic
 
 export { PhotoManipulator };
