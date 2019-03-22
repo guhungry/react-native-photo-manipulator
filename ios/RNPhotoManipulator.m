684cdbcc7105581a1a9f36127fb7ed942f681628
Woraphot Chokratanasombat <guhungry@gmail.com>
2019-03-23 01:18:54 +0700

    [ios] Change: Add blank implementation

--- ios/RNPhotoManipulator.m
+++ ios/RNPhotoManipulator.m
@@ -9,5 +9,47 @@
 }
 RCT_EXPORT_MODULE()
 
+RCT_EXPORT_METHOD(batch:(NSString *)uri
+                  size:(NSDictionary *)size
+                  quality:(NSInteger)quality
+                  operations:(NSArray *)operations
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    resolve(uri);
+}
+
+RCT_EXPORT_METHOD(overlayImage:(NSString *)uri
+                  icon:(NSArray *)icon
+                  position:(NSDictionary *)position
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    resolve(uri);
+}
+
+RCT_EXPORT_METHOD(printText:(NSString *)uri
+                  list:(NSArray *)list
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    resolve(uri);
+}
+
+RCT_EXPORT_METHOD(optimize:(NSString *)uri
+                  quality:(NSInteger)quality
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    resolve(uri);
+}
+
+RCT_EXPORT_METHOD(resize:(NSString *)uri
+                  targetSize:(NSDictionary *)targetSize
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    resolve(uri);
+}
+
 @end
-  
