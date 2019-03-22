960e2446fa21a20e55b5d790a07125c64a6d43b2
Woraphot Chokratanasombat <bee.w@excelbangkok.com>
2019-03-22 10:12:36 +0700

    [RN Photo Manipulator Package] Change: Add createJSModules() for backward compatibility

--- android/src/main/java/com/guhungry/photomanipulator/RNPhotoManipulatorPackage.java
+++ android/src/main/java/com/guhungry/photomanipulator/RNPhotoManipulatorPackage.java
@@ -16,6 +16,11 @@ public class RNPhotoManipulatorPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNPhotoManipulatorModule(reactContext));
     }
 
+    // Deprecated from RN 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
