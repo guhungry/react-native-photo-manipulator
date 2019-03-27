7f8f568b46548c57e3c5953887b78167fe646bcb
Woraphot Chokratanasombat <bee.w@excelbangkok.com>
2019-03-27 10:11:32 +0700

    [Param Utils] Fix: Can't get image source from jsbundle (from require())

--- src/ParamUtils.ts
+++ src/ParamUtils.ts
@@ -1,25 +1,35 @@
 import { Image } from "react-native";
-import { Color, PhotoBatchOperations, TextOptions } from "./PhotoManipulatorTypes";
 // @ts-ignore
 import parse from "parse-color";
+import { Color, ImageSource, PhotoBatchOperations, TextOptions } from "./PhotoManipulatorTypes";
 
 /**
  * Parameter Utilities Class
  */
 export class ParamUtils {
     static toBatchNative = (it: PhotoBatchOperations) => {
-        if (it.operation === "text") return { ...it, options: ParamUtils.toTextOptionsNative(it.options) }
-        if (it.operation === "overlay") return { ...it, overlay: typeof it.overlay === "string" ? it : Image.resolveAssetSource(it.overlay).uri }
+        if (it.operation === "text") return { ...it, options: ParamUtils.toTextOptionsNative(it.options) };
+        if (it.operation === "overlay") return { ...it, overlay: ParamUtils.toImageNative(it.overlay) };
         return it
-    }
-    static toTextOptionsNative = (it: TextOptions) => ({ ...it, color: ParamUtils.toColorNative(it.color), thickness: it.thickness || 0 })
+    };
+
+    static toTextOptionsNative = (it: TextOptions) => ({ ...it, color: ParamUtils.toColorNative(it.color), thickness: it.thickness || 0 });
+
     static toColorNative = (color?: string): Color => {
-        const result = parse(color || "#000000").rgba
+        const result = parse(color || "#000000").rgba;
         return {
             r: result[0],
             g: result[1],
             b: result[2],
             a: result[3] * 255,
         }
+    };
+
+    static toImageNative = (source: ImageSource) => {
+        if (typeof source === "string") return source;
+
+        const {uri} = Image.resolveAssetSource(source);
+        if (uri.includes("://")) return uri;
+        return `file:///android_res/${uri}`
     }
 }
