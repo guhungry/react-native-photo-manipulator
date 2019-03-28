bef2d7ec360bd6c69103cf2e85983ed03b4d9df2
Woraphot Chokratanasombat <bee.w@excelbangkok.com>
2019-03-28 12:05:39 +0700

    [Photo Manipulator] Change:  Use [] instead of Array<>

--- src/PhotoManipulator.ts
+++ src/PhotoManipulator.ts
@@ -12,11 +12,11 @@ import {ParamUtils} from "./ParamUtils";
 const { RNPhotoManipulator } = NativeModules;
 
 const PhotoManipulator: PhotoManipulatorStatic = {
-    batch: (image: ImageSource, size: Size, quality: number, operations: Array<PhotoBatchOperations>) => {
+    batch: (image: ImageSource, size: Size, quality: number, operations: PhotoBatchOperations[]) => {
         return RNPhotoManipulator.batch(image, size, quality, operations.map(ParamUtils.toBatchNative))
     },
     overlayImage: RNPhotoManipulator.overlayImage,
-    printText: (image: ImageSource, texts: Array<TextOptions>) => RNPhotoManipulator.printText(image, texts.map(ParamUtils.toTextOptionsNative)),
+    printText: (image: ImageSource, texts: TextOptions[]) => RNPhotoManipulator.printText(image, texts.map(ParamUtils.toTextOptionsNative)),
     optimize: RNPhotoManipulator.optimize,
     resize: RNPhotoManipulator.resize
 };
