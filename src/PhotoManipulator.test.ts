3b33ff0922e13738ebd86d6ffe47304bf2125634
Woraphot Chokratanasombat <bee.w@excelbangkok.com>
2019-04-10 15:52:16 +0700

    [Test] Change: Add test for PhotoManipulator.batch()

--- src/PhotoManipulator.test.ts
+++
@@ -1,7 +0,0 @@
-import { NativeModules } from "react-native";
-
-describe("Photo Manipulator", () => {
-    test("Native Module Mock", () => {
-        expect(NativeModules.RNPhotoManipulator).toBeObject();
-    });
-});
