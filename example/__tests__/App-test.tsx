c09cb1379df6940037a837e69b4a274d6a06dc17
Woraphot Chokratanasombat <guhungry@gmail.com>
2019-04-11 22:46:57 +0700

    [Typescript] Fix: Typescript warnings

--- example/__tests__/App-test.tsx
+++ example/__tests__/App-test.tsx
@@ -3,11 +3,11 @@
  */
 
 import 'react-native';
-import React from 'react';
+import * as React from 'react';
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import * as renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
