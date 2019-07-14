85e1079b50edbc9a1a552dcb095229a7b4f6faae
Woraphot Chokratanasombat <guhungry@gmail.com>
2019-07-14 10:44:00 +0700

    [Example] Change: Make detox test pass

--- example/e2e/firstTest.spec.js
+++ example/e2e/firstTest.spec.js
@@ -4,16 +4,6 @@ describe('Example', () => {
   });
 
   it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
-  });
-
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
-
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+    await expect(element(by.text('Welcome to React'))).toBeVisible();
   });
 });
