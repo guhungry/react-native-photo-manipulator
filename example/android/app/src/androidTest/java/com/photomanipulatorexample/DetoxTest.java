c90836cb723563b7308e5b321d896b6ee3272bf0
Woraphot Chokratanasombat <guhungry@gmail.com>
2020-07-11 01:06:43 +0700

    [Example] Change: Update to React Native 0.63.0

--- example/android/app/src/androidTest/java/com/photomanipulatorexample/DetoxTest.java
+++ example/android/app/src/androidTest/java/com/photomanipulatorexample/DetoxTest.java
@@ -15,10 +15,16 @@ import androidx.test.rule.ActivityTestRule;
 public class DetoxTest {
 
     @Rule
+    // Replace 'MainActivity' with the value of android:name entry in
+    // <activity> in AndroidManifest.xml
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class, false, false);
 
     @Test
     public void runDetoxTests() {
-        Detox.runTests(mActivityRule);
+        Detox.DetoxIdlePolicyConfig idlePolicyConfig = new Detox.DetoxIdlePolicyConfig();
+        idlePolicyConfig.masterTimeoutSec = 60;
+        idlePolicyConfig.idleResourceTimeoutSec = 30;
+
+        Detox.runTests(mActivityRule, idlePolicyConfig);
     }
 }
