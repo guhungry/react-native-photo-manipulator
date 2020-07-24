1bf7df7e6339765c646586430f1bac4f3fa2b37b
Woraphot Chokratanasombat <guhungry@gmail.com>
2020-07-25 00:10:59 +0700

    [Example] Change: Use demo images from github repository

--- example/app/example/settings.ts
+++ example/app/example/settings.ts
@@ -1,2 +1,2 @@
-export const IMAGE = "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?cs=srgb&dl=beauty-bloom-blue-67636.jpg&fm=jpg?dl&fit=crop&crop=entropy&w=800&h="
-export const OVERLAY = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/200px-React-icon.svg.png"
+export const IMAGE = 'https://github.com/guhungry/react-native-photo-manipulator/raw/master/docs/demo-background.jpg';
+export const OVERLAY = 'https://github.com/guhungry/react-native-photo-manipulator/raw/master/docs/demo-icon.png';
