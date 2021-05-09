991d40f3b22c8275d57c441a268e17d94d7531ec
Woraphot Chokratanasombat <guhungry@gmail.com>
2021-05-09 10:37:54 +0700

    [Example@Batch] Change: Add crop only, add more overlays
    Also update to react-native-photo-manipulator:1.2.3

--- example/app/example/ExampleBatch.tsx
+++ example/app/example/ExampleBatch.tsx
@@ -13,27 +13,28 @@ export default React.memo(function ExampleBatch() {
   React.useEffect(() => {
     const operation = async () => {
       setImage(await PhotoManipulator.batch(IMAGE, operations(), { x: 40, y: 5, width: 600, height: 400 }, { width: 300, height: 200 }, 30))
-      setImagePng(await PhotoManipulator.batch(IMAGE, operations(), { x: 40, y: 5, width: 600, height: 400 }, { width: 300, height: 200 }, 30, MimeType.PNG))
+      setImagePng(await PhotoManipulator.batch(IMAGE, operations(), { x: 40, y: 5, width: 600, height: 400 }, undefined, undefined, MimeType.PNG))
     };
 
     operation().then(noop).catch(console.log);
   }, []);
 
   return <>
-    { typeof image === "string" ? <Text style={styles.exampleDescription}>JPEG</Text> : null }
-    { typeof image === "string" ? <Image testID="batchResult" style={styles.image} source={{ uri: image}} /> : null }
-    { typeof imagePng === "string" ? <Text style={styles.exampleDescription}>PNG</Text> : null }
-    { typeof imagePng === "string" ? <Image testID="batchResultPng" style={styles.image} source={{ uri: imagePng}} /> : null }
-    </>;
+      { typeof image === "string" ? <Text style={styles.exampleDescription}>Crop & Resize</Text> : null }
+      { typeof image === "string" ? <Image testID="batchResult" style={styles.image} source={{ uri: image}} /> : null }
+      { typeof imagePng === "string" ? <Text style={styles.exampleDescription}>Crop Only</Text> : null }
+      { typeof imagePng === "string" ? <Image testID="batchResultPng" style={styles.image} source={{ uri: imagePng}} /> : null }
+    </>
 
   function operations(): PhotoBatchOperations[] {
     return [
       { operation: "text", options: { text: "Test Print Text", color: "black", textSize: 30, position: { x: 80, y: 30 }, thickness: 4 } },
       { operation: "text", options: { text: "Test Print Text", color: "white", textSize: 30, position: { x: 80, y: 30 } } },
+      { operation: "overlay", overlay: OVERLAY, position: { x: 110, y: 25 } },
       { operation: "text", options: { text: "Test Print Text 2", color: "blue", textSize: 30, position: { x: 100, y: 80 }, thickness: 4 } },
       { operation: "text", options: { text: "Test Print Text 2", color: "brown", textSize: 30, position: { x: 100, y: 80 } } },
       { operation: "text", options: { text: "Test Print Text 3", color: "green", textSize: 30, position: { x: 180, y: 180 } } },
-      { operation: "overlay", overlay: OVERLAY, position: { x: 30, y: 65 } },
+      { operation: "overlay", overlay: OVERLAY, position: { x: 15, y: 65 } },
     ]
   }
 })
