deb6d51b2a841202d03ce8d51b6d4715236db167
Woraphot Chokratanasombat <guhungry@gmail.com>
2024-07-21 19:36:36 +0700

    [Android] Fix: Native module didn't compiled in React Native 0.64.4

--- android/src/main/java/com/guhungry/rnphotomanipulator/RNPhotoManipulatorPackage.java
+++ android/src/main/java/com/guhungry/rnphotomanipulator/RNPhotoManipulatorPackage.java
@@ -4,16 +4,35 @@ package com.guhungry.rnphotomanipulator;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.facebook.react.ReactPackage;
 import com.facebook.react.TurboReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.module.model.ReactModuleInfo;
 import com.facebook.react.module.model.ReactModuleInfoProvider;
+import com.facebook.react.uimanager.ViewManager;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class RNPhotoManipulatorPackage extends TurboReactPackage {
+public class RNPhotoManipulatorPackage extends TurboReactPackage implements ReactPackage {
+    @NonNull
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new RNPhotoManipulatorModule(reactContext));
+        return modules;
+    }
+
+    @NonNull
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
     @Nullable
     @Override
     public NativeModule getModule(@NonNull String name, @NonNull ReactApplicationContext reactContext) {
